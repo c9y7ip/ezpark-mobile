cc05aecb478193d453e48e1cb1daa8c79ceb34df
Yip Wai Si <waisiy@sfu.ca>
2020-11-07 01:59:44 -0800

    delete importing async-storage separetely and debug.log

--- src/components/Auth/LoginScreen.js
+++ src/components/Auth/LoginScreen.js
@@ -20,7 +20,7 @@ const LoginScreen = ({ navigation }) => {
 
 
   const connect =  () => {
-    axios.post('http://localhost:5000/auth/login',{
+    axios.post('http://192.168.0.13:5000/auth/login',{
       email:email,
       password:password})
       .then(async (tok)=>{
