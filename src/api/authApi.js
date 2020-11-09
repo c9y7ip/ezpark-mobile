fa51005fa5c9622510630ec35ab958e1a0d38da6
Jason <jasonwu@Jasons-MBP.hitronhub.home>
2020-11-08 17:44:51 -0800

    refactor api calls

---
+++ src/api/authApi.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+// swap baseURL here so that it applies to all api requests.
+// const baseURL = 'http://localhost:5000'
+const baseURL = 'http://35.202.57.20:5000'
+
+const login = (email, password) => {
+    console.log('logging in')
+    return axios.post(`${baseURL}/auth/login`, {
+        email: email,
+        password: password
+    }).then(response => {
+        console.log(response)
+        return response.data
+    })
+        .catch(err => {
+            console.log(err)
+            throw err
+        })
+}
+
+const register = (name, password, email, phone) => {
+    return axios.post(`${baseURL}/auth/register`, {
+        name: name,
+        password: password,
+        email: email,
+        phone: phone,
+        isAdmin: false
+    }).then(response => response.data)
+        .catch(err => {
+            console.log(err)
+            throw err
+        })
+}
+
+module.exports = {
+    login: login,
+    register: register
+}
